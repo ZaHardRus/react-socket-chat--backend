4f289f7849bef92df0dd38eec0f3a858e65fec77
Denis Ackerman <zahardrus@gmail.com>
2021-10-01 19:18:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express();
+const server = require('http').createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server);
+const PORT = 9999
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const rooms = new Map()
+app.get('/rooms/:id', (req, res) => {
+    const roomId = req.params.id
+    const obj = rooms.has(roomId)
+        ? {
+            users: [...rooms.get(roomId).get('users').values()],
+            messages: [...rooms.get(roomId).get('messages').values()]
+        }
+        : {users: [], messages: []}
+    res.json(obj)
+});
+app.post('/rooms', (req, res) => {
+    const {roomId, userName} = req.body
+    if (!rooms.has(roomId)) {
+        rooms.set(
+            roomId,
+            new Map([
+                ['users', new Map()],
+                ['messages', []]
+            ])
+        )
+    }
+    res.send([...rooms.keys()])
+
+})
+
+io.on('connection', (socket) => {
+    socket.on('ROOM:JOIN', ({roomId, userName}) => {
+        socket.join(roomId)
+        rooms.get(roomId).get('users').set(socket.id, userName)
+        const users = [...rooms.get(roomId).get('users').values()]
+        socket.to(roomId).emit('ROOM:JOINED', users)
+    })
+    socket.on('disconnect', () => {
+        rooms.forEach((value, roomId) => {
+            if (value.get('users').delete(socket.id)) {
+                const users = [...rooms.get(roomId).get('users').values()]
+                socket.broadcast.to(roomId).emit('ROOM:LEAVE', users)
+            }
+        })
+    })
+    socket.on('ROOM:NEW_MESSAGE', ({roomId, userName, text}) => {
+        const obj = {userName,text,date:Date.now()}
+        rooms.get(roomId).get('messages').push(obj)
+        socket.broadcast.to(roomId).emit('ROOM:NEW_MESSAGE', obj)
+    })
+});
+
+server.listen(PORT, () => {
+    console.log('listening on *:' + PORT);
+});
